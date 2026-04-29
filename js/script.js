ed422b74d7a9c4fcbf72f6b42c739e12c027d097
SOUFIANE <elvetta26@gmail.com>
2026-04-29 11:07:42 +0200

    Fix: random change png to jpg

--- js/script.js
+++ js/script.js
@@ -7,8 +7,26 @@ const imagenes = [
     "5.jpg",
     "6.jpg",
 ]
+
+let random = Math.floor((Math.random() * 5) + 1)
+
+const welcomeImgContainer = document.querySelector("#welcomeImgContainer")
+const mainImg = document.createElement('img')
+mainImg.src = `assets/${random}.jpg`;
+mainImg.id = "mainImg";
+mainImg.title = "titulo de imagen";
+mainImg.alt="es una imagen";
+
+welcomeImgContainer.append(mainImg);
+
+
+
+
+
 const discover = document.querySelector("#discover")
 const fragment = document.createDocumentFragment()
+
+
 
 imagenes.forEach((imagen, indice) => {
     const img = document.createElement('img');
